c46efb5e5c72e514cb985dfb4ac04ce1026d3cc6
MircQ <56171999+MircQ@users.noreply.github.com>
2020-01-16 22:04:37 +0100

    update login page

--- Gamebase/src/main/java/graphicInterface/GraphicInterface.java
+++ Gamebase/src/main/java/graphicInterface/GraphicInterface.java
@@ -7,18 +7,42 @@ import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
 import java.awt.Color;
+import java.awt.Cursor;
+
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+
+import databaseConnection.MongoConnector;
+import utilities.UserTableType;
+import utilities.UserType;
+
 import javax.swing.JPasswordField;
 import javax.swing.JButton;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
 
 public class GraphicInterface {
 
 	private JFrame frame;
+	private JPanel panel;
+	
+	//Login Page
+	private JPanel loginPanel;
+	private JLabel usernameLabel;
+	private JLabel passwordLabel;
 	private JTextField usernameTextfield;
 	private JPasswordField passwordField;
+	private JButton signUpButton;
+	private JButton loginButton;
+	private JLabel errorMessageLabel;
+	private JLabel myGamesLabel;
 
+	//Home page
+	private JPanel homePagePanel;
+	
 	/**
 	 * Launch the application.
 	 */
@@ -50,56 +74,155 @@ public class GraphicInterface {
 		frame.setBounds(100, 100, 952, 615);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		JPanel panel = new JPanel();
+		panel = new JPanel();
 		frame.getContentPane().add(panel, BorderLayout.CENTER);
 		panel.setLayout(new CardLayout(0, 0));
 		
-		JPanel loginPanel = new JPanel();
+		loginPanel = new JPanel();
 		loginPanel.setName("loginPanel");
 		loginPanel.setBackground(new Color(87, 86, 82));
-		panel.add(loginPanel, "name_2983883636122500");
+		panel.add(loginPanel, "loginPanel");
 		loginPanel.setLayout(null);
 		
-		JLabel usernameLabel = new JLabel("Username");
+		usernameLabel = new JLabel("Username");
 		usernameLabel.setFont(new Font("Corbel", Font.BOLD, 20));
 		usernameLabel.setName("usernameLabel");
 		usernameLabel.setForeground(Color.WHITE);
-		usernameLabel.setBounds(175, 267, 166, 25);
+		usernameLabel.setBounds(269, 254, 166, 25);
 		loginPanel.add(usernameLabel);
 		
-		JLabel passwordLabel = new JLabel("Password");
+		passwordLabel = new JLabel("Password");
+		passwordLabel.setFont(new Font("Corbel", Font.BOLD, 20));
 		passwordLabel.setForeground(Color.WHITE);
 		passwordLabel.setName("passwordLabel");
-		passwordLabel.setBounds(172, 367, 56, 16);
+		passwordLabel.setBounds(269, 342, 107, 25);
 		loginPanel.add(passwordLabel);
 		
 		usernameTextfield = new JTextField();
-		usernameTextfield.setBounds(163, 295, 394, 37);
+		usernameTextfield.setBounds(269, 275, 387, 43);
 		loginPanel.add(usernameTextfield);
 		usernameTextfield.setColumns(10);
 		
 		passwordField = new JPasswordField();
-		passwordField.setBounds(163, 402, 394, 37);
+		passwordField.setBounds(269, 362, 387, 43);
 		loginPanel.add(passwordField);
 		
-		JButton signUpButton = new JButton("Sign Up");
+		signUpButton = new JButton("Sign Up");
+		signUpButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		signUpButton.setBorderPainted(false);
+		signUpButton.setBackground(new Color(0, 128, 128));
+		signUpButton.setOpaque(false);
+		signUpButton.setContentAreaFilled(false);
+		signUpButton.setBorder(null);
+		signUpButton.setForeground(new Color(0, 0, 128));
+		signUpButton.setFont(new Font("Corbel", Font.BOLD, 21));
 		signUpButton.setToolTipText("Click Here To Sign Up");
 		signUpButton.setName("signUpButton");
-		signUpButton.setBounds(290, 483, 97, 25);
+		signUpButton.setBounds(328, 470, 107, 38);
+		signUpButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				CardLayout cl = (CardLayout)(panel.getLayout());
+				
+				String username = usernameTextfield.getText();
+				String password = new String(passwordField.getPassword());
+				
+				System.out.println("GRAPHICINTERFACE.JAVA/SIGNUPACTIONPERFORMED-->trying to sign up " + username);
+				
+				if( username.equals("") || password.equals("") ) {
+					System.out.println("GRAPHICINTERFACE.JAVA/SIGNUPACTIONPERFORMED-->sign up failed: empty username and(or) password");
+					errorMessageLabel.setText("Please Insert Username and Password");
+					errorMessageLabel.setVisible(true);
+					return;
+				}
+				
+				if( !MongoConnector.signUp(username,password) ) {
+					System.out.println("GRAPHICINTERFACE.JAVA/SIGNUPACTIONPERFORMED-->sign up failed: username " + username + " already exists");
+					errorMessageLabel.setText("Username already used");
+					errorMessageLabel.setVisible(true);
+					return;
+				} else {
+					System.out.println("GRAPHICINTERFACE.JAVA/LOGINACTIONPERFORMED-->sign up completed: username " + username + " registered");
+					cl.show(panel, "homePagePanel");
+					
+				}
+			}
+		});
 		loginPanel.add(signUpButton);
 		
-		JButton loginButton = new JButton("Login");
+		loginButton = new JButton("Login");
+		loginButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		loginButton.setBorderPainted(false);
+		loginButton.setBackground(new Color(0, 128, 128));
+		loginButton.setOpaque(false);
+		loginButton.setContentAreaFilled(false);
+		loginButton.setBorder(null);
+		loginButton.setForeground(Color.WHITE);
+		loginButton.setFont(new Font("Corbel", Font.BOLD, 21));
 		loginButton.setName("loginButton");
-		loginButton.setBounds(481, 483, 97, 25);
+		loginButton.setBounds(489, 470, 103, 38);
+		loginButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				CardLayout cl = (CardLayout)(panel.getLayout());
+				
+				String username = usernameTextfield.getText();
+				String password = new String(passwordField.getPassword());
+				
+				System.out.println("GRAPHICINTERFACE.JAVA/LOGINACTIONPERFORMED-->user " + username + " is trying to login");
+				
+				if( username.equals("") || password.equals("") ) {
+					System.out.println("GRAPHICINTERFACE.JAVA/LOGINACTIONPERFORMED-->empty username and(or) password");
+					errorMessageLabel.setText("Please Insert Username and Password");
+					errorMessageLabel.setVisible(true);
+					return;
+				}
+				
+				UserType usertype = MongoConnector.login(username,password);
+				
+				switch(usertype) {
+					case ADMINISTRATOR:
+						System.out.println("GRAPHICINTERFACE.JAVA/LOGINACTIONPERFORMED-->login completed:user " + username + " logged in");
+						cl.show(panel, "homePagePanel");
+						
+						break;
+					case USER:
+						System.out.println("GRAPHICINTERFACE.JAVA/LOGINACTIONPERFORMED-->login completed:user " + username + " logged in");
+						cl.show(panel, "homePagePanel");
+						
+						break;
+					case NOUSER:
+						System.out.println("GRAPHICINTERFACE.JAVA/LOGINACTIONPERFORMED-->login failed: no user " + username + " found");
+						errorMessageLabel.setText("No User " + username + " found");
+						errorMessageLabel.setVisible(true);
+						break;
+					case WRONGPASSWORD:
+						System.out.println("GRAPHICINTERFACE.JAVA/LOGINACTIONPERFORMED-->login failed: wrong password for username " + username);
+						errorMessageLabel.setText("Uncorrect Password for User " + username );
+						errorMessageLabel.setVisible(true);
+						break;
+					default:
+						return;
+				}
+			}
+		});
 		loginPanel.add(loginButton);
 		
-		JLabel errorMessageLabel = new JLabel("");
+		errorMessageLabel = new JLabel("");
+		errorMessageLabel.setForeground(new Color(255, 0, 0));
+		errorMessageLabel.setFont(new Font("Corbel", Font.PLAIN, 17));
 		errorMessageLabel.setVisible(false);
 		errorMessageLabel.setName("errorMessageLabel");
-		errorMessageLabel.setBounds(720, 370, 56, 16);
+		errorMessageLabel.setBounds(269, 418, 387, 31);
 		loginPanel.add(errorMessageLabel);
 		
-		JPanel homePagePanel = new JPanel();
-		panel.add(homePagePanel, "name_2983995321266200");
+		myGamesLabel = new JLabel("");
+		myGamesLabel.setIcon(new ImageIcon(GraphicInterface.class.getResource("/resources/mygames.png")));
+		myGamesLabel.setName("myGamesLabel");
+		myGamesLabel.setBounds(304, 51, 318, 170);
+		loginPanel.add(myGamesLabel);
+		
+		homePagePanel = new JPanel();
+		panel.add(homePagePanel, "homePagePanel");
 	}
 }
