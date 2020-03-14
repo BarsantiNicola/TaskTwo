a566e5b1b836d61b8c4ddf48b7cace3b6f52e18b
Riccardo Bertini <m_bertini@hotmail.com>
2020-03-14 15:36:28 +0100

    Small Update (x Nicola)

---
+++ Gamebase/src/main/java/logic/data/UserInfo.java
@@ -0,0 +1,40 @@
+package logic.data;
+
+import logic.StatusCode;
+
+/* Returned at login attempts */
+
+public class UserInfo
+ {
+  public StatusCode statusCode;
+  public UserType userType;
+  public User user;
+  
+  public UserInfo()
+   {
+    this.statusCode = StatusCode.ERR_GRAPH_UNKNOWN;
+    this.userType = UserType.NO_USER;
+    user = null;
+   }
+  
+  public UserInfo(StatusCode status)
+   {
+   this.statusCode = status;
+   this.userType = UserType.NO_USER;
+   user = null;
+  }
+  
+  public UserInfo(StatusCode status, UserType userType, User user)
+   {
+   this.statusCode = status;
+   this.userType = userType;
+   this.user = user;
+  }
+  
+  @Override
+  public String toString()
+   {
+    return "StatusCode = "+ statusCode + ", UserType = " + userType + "\n" + user;
+   }
+  
+ }
